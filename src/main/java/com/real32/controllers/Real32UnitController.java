b464d90ea380a00603f2be57c3271dccacfbd48c
Alistair Haslam <u1773783@unimail.hud.ac.uk>
2021-06-15 15:12:16 +0100

    Assembly now works
    Real 32 assembly works!

--- src/main/java/com/real32/controllers/Real32UnitController.java
+++ src/main/java/com/real32/controllers/Real32UnitController.java
@@ -5,22 +5,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.real32.repositories.MountRepository;
 import com.real32.repositories.Real32UnitRepository;
 import com.real32.services.CustomUserDetailsService;
 
 @Controller
 public class Real32UnitController {
-
-	@Autowired
-	private CustomUserDetailsService userService;
 	
 	@Autowired
-	private Real32UnitRepository real32UnitRespository;
+	private MountRepository mountRepository;
 	
-	@GetMapping(value = "/Real32")
+	@GetMapping(value = "/real32")
 	public ModelAndView real32Unit()
 	{
 		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.addObject("mounts", mountRepository.findAll());
 		modelAndView.setViewName("Real32");
 		return modelAndView;
 	}
